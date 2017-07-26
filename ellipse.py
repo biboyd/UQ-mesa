9eb83a9da572ea6763f089f7b71d6b9a672a2772
dwillcox <eugene.willcox@gmail.com>
2017-07-25 22:24:44 -0400

    modified intervalshapes plot

--- ellipse.py
+++ ellipse.py
@@ -10,16 +10,16 @@ import matplotlib.patches as patches
 # set number of points and ellipse size, based on interval
 
 N = 200
-xmax = 0.9
-xmin = 0.3
-ymax = 0.1
-ymin = 0.01
+ymax = 0.9
+ymin = 0.3
+xmax = 0.1
+xmin = 0.01
 
 a = ( xmax - xmin )/2.
 b = ( ymax - ymin )/2.
 
-print("X interval is: [%s,%s]") % (xmax, xmin)
-print("Y interval is: [%s,%s]") % (ymax, ymin)
+print("X interval is: [{},{}]".format(xmax, xmin))
+print("Y interval is: [{},{}]".format(ymax, ymin))
 
 #--------------------------------#
 #           METHOD 1             #
@@ -67,8 +67,7 @@ y3 = (ymin + b) + (np.sqrt(2.) * b * r * np.sin(theta))
 
 # Plots 
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
+fig, ax = plt.subplots(figsize=(8,6))
 
 #ax.add_patch(patches.Ellipse((xmin+a,ymin+b),2. * a,2. * b,alpha=0.2, label='Inscribed Ellipse'))
 #ax.add_patch(patches.Rectangle((xmin,ymin),2. *a,2. *b,color='g',alpha=0.1, label='Square Interval' ))
@@ -83,16 +82,16 @@ ax.add_patch(patches.Ellipse((xmin+a,ymin+b),2. * a,2. * b, facecolor='#a6bddb',
 #ax.scatter(x,y,c='r')
 #ax.scatter(x2,y2,c='b')
 #ax.scatter(x3,y3,c='g')
-ax.legend(fontsize='medium')
+ax.legend(fontsize=18)
 
-ax.text(0.6,0.05,r'$\frac{x^2}{(\Delta x /2)^2} + \frac{y^2}{(\Delta y /2)^2} = 1$',horizontalalignment='center',verticalalignment='center',fontsize=15.0)
-ax.text(0.6,0.109,r'$\frac{x^2}{(\sqrt{2}\Delta x)^2} + \frac{y^2}{(\sqrt{2}\Delta y)^2} = 1$',horizontalalignment='center',verticalalignment='center',fontsize=15.0)
-ax.text(0.3,0.01,r'$[\Delta x,\Delta y]$',horizontalalignment='left',verticalalignment='bottom',fontsize=13.0)
+ax.text(0.05, 0.6, r'$\frac{x_1^2}{\Delta_1^2} + \frac{x_2^2}{\Delta_2^2} = 1$',horizontalalignment='center',verticalalignment='center',fontsize=24)
+ax.text(0.05, 0.95, r'$\frac{x_1^2}{(\sqrt{2}\Delta_1)^2} + \frac{x_2^2}{(\sqrt{2}\Delta_2)^2} = 1$',horizontalalignment='center',verticalalignment='center',fontsize=24)
+ax.text(0.01, 0.3, r'$[2 \Delta_1,\; 2 \Delta_2]$',horizontalalignment='left',verticalalignment='bottom',fontsize=18)
 
-plt.xlim([0.1,1.1])
-plt.ylim([-0.02,0.15])
-plt.xlabel('Parameter 1, $\eta_{R}$')
-plt.ylabel('Parameter 2, $\eta_{B}$')
-plt.title('Elliptical Constraints and Square Interval')
-plt.savefig('intervalshapes.eps', format='eps', dpi=1200)
+plt.ylim([0.1,1.4])
+plt.xlim([-0.02,0.13])
+plt.xlabel('$x_1 = \eta_B$')
+plt.ylabel('$x_2 = \eta_R$')
+#plt.title('Elliptical Constraints and Square Interval')
+plt.savefig('intervalshapes_v2.eps', format='eps', dpi=1200)
 plt.show()
