80019510bad494e704b245c4e55afcfcf26d007e
Donald E. Willcox <eugene.willcox@gmail.com>
2019-12-13 14:39:42 -0800

    Add sample commands for running create_mesa_samples.py

---
+++ create_uq_samples.sh
@@ -0,0 +1,1 @@
+python3 create_mesa_samples.py -name test_samples -tdir template_directory -tinlist inlist_1.0 -n 256 -u -c -e -lo 0.3 0.01 -hi 0.9 0.1 -p Reimers_scaling_factor Blocker_scaling_factor
